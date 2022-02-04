5ffcd43c0bc72e94faeb88a09d3cd6185b49764f
MorenaMastromarino <morena.mastromarino98@gmail.com>
2022-02-04 17:06:32 +0100

    category in create

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 <div class="container">
   <h1>{{$post->title}}</h1>
+  @if ($post->category)
+    <h4>Categoria: {{$post->category->name}}</h4>
+  @endif
   <p>{{$post->content}}</p>
 
   <div class="row">
